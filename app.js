466abe121e8e27c661233f71e7ad96932d0517b9
lewis <lewis@evergreen.co>
2016-08-22 14:34:47 +0100

    Basic Handlebars (hbs) integration

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'hbs');
+app.engine('html', require('hbs').__express);
+
 app.get('/', function (req, res) {
-  res.send('Welcome to my blog');
+  res.render('index');
 });
 
 app.listen(3000, function () {
