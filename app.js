dab5721fae9d4fc4f78b10e8e57c2dd999662214
Lewis White <lewis@lody.be>
2016-08-22 19:12:38 +0100

    Template engine setup

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var express = require('express');
+var hbs = require('hbs');
+
 var app = express();
 
 app.set('view engine', 'hbs');
 app.engine('html', require('hbs').__express);
+app.use(express.static('public'));
 
+var stacks = {};
+
+hbs.registerHelper('push', function(name, context) {
+    var stack = stacks[name];
+    if (!stack) {
+        stack = stacks[name] = [];
+    }
+
+    stack.push(context.fn(this)); // for older versions of handlebars, use stack.push(context(this));
+});
+
+hbs.registerHelper('stack', function(name) {
+    var val = (stacks[name] || []).join('\n');
+
+    // clear the stack
+    stacks[name] = [];
+    return val;
+})
+;
 app.get('/', function (req, res) {
-  res.render('index');
+    res.render('index');
 });
 
 app.listen(3000, function () {
-  console.log('Server initialised: http://localhost:3000/');
+    console.log('Server initialised: http://localhost:3000/');
 });
