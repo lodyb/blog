8035539bcc85a6a0d10f8b25c86572c7b80423a0
Lewis White <lewis@lody.be>
2016-08-24 22:26:14 +0100

    Added working gallery implementation (placeholder)

--- app.js
+++ app.js
@@ -8,6 +8,53 @@ app.engine('html', require('hbs').__express);
 app.use(express.static('public'));
 
 var stacks = {};
+var articles = [
+        {
+            "title": "Test 1",
+            "href": "google.com",
+            "bg": "test"
+        },
+        {
+            "title": "Test 2",
+            "href": "google.com",
+            "bg": "test2"
+        },
+        {
+            "title": "Test 3",
+            "href": "google.com",
+            "bg": "test"
+        },
+        {
+            "title": "Test 4",
+            "href": "google.com",
+            "bg": "test"
+        },
+        {
+            "title": "Test 5",
+            "href": "google.com",
+            "bg": "test2"
+        },
+        {
+            "title": "Test 6",
+            "href": "google.com",
+            "bg": "test"
+        },
+        {
+            "title": "Test 7",
+            "href": "google.com",
+            "bg": "test"
+        },
+        {
+            "title": "Test 8",
+            "href": "google.com",
+            "bg": "test2"
+        },
+        {
+            "title": "Test 9",
+            "href": "google.com",
+            "bg": "test"
+        }
+];
 
 hbs.registerHelper('push', function(name, context) {
     var stack = stacks[name];
@@ -25,7 +72,11 @@ hbs.registerHelper('stack', function(name) {
 });
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {
+        title: 'My favorite veggies',
+        figure: articles,
+        layout: 'layout'
+    });
 });
 
 app.listen(3000, function () {
